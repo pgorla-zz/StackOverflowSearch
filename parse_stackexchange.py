2c24c7c55304458c1165234a64287aa910a9ee02
Patricia Gorla <pgorla@o19s.com>
2012-11-10 22:04:17 +0000

    Parse HTML out of Body.

--- parse_stackexchange.py
+++ parse_stackexchange.py
@@ -2,34 +2,38 @@
 
 """
 Each StackExchange folder contains 6 XML files: badges, comments,
-posts, posthistory, users, votes.
+posts, posthistory, users, votes. We are only interested in posts.
+And perhaps comments.
 
-This script enters into the specified directory and combines the data 
-into an xml file readable by Solr.
+This script reads and processes the data from posts.xml.
+
+Relevant Fields:
+    Body, CreationDate, ClosedDate, FavoriteCount, Id, Score, Tags, 
+    Title, ViewCount
 
 """
 
 from lxml import etree
+
+from bs4 import BeautifulSoup
 from pandas import DataFrame
 
 
 DATADIR = "scifidata/"
 
+xml = "posts.xml"
 
-class Document(Object):
-""" Generate a DataFrame from xml. """
+doc = etree.parse("%s%s" % (DATADIR, xml))
+posts = [dict(e.items()) for e in doc.getiterator() 
+    if e.items() != []]
+df = DataFrame(posts)
 
-    def __init__(self, xml):
-        doc = etree.parse("%s%s" % (DATADIR, xml))
-        self.dictitems = [dict(e.items()) for e in doc.getiterator()]
-        self.df = DataFrame(self.dictitems)
+def decode_body(elem):
+    try:
+        elem = elem.encode('utf-8')
+        return ''.join(BeautifulSoup(elem).findAll(text=True)
+    except AttributeError:
+        # This means an empty value got through.
+        return None
 
-doc = etree.parse("scifidata/posts.xml")
-root = doc.getroot()
-
-# Generates a list of all items for every row.
-posts = [dict(elem.items()) for elem in doc.getiterator()]
-
-# Might not need to specify index.
-df = DataFrame(posts, index=range(len(posts)))
-
+df['Body'] = df['Body'].map(decode_body)
