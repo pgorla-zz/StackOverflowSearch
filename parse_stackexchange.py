a50e134cf9e89d94fb829ef1c39f24506d14ab00
Patricia Gorla <pgorla@o19s.com>
2012-11-11 14:18:24 +0000

    Encode data in utf-8 and write to csv.

--- parse_stackexchange.py
+++ parse_stackexchange.py
@@ -24,16 +24,47 @@ DATADIR = "scifidata/"
 xml = "posts.xml"
 
 doc = etree.parse("%s%s" % (DATADIR, xml))
-posts = [dict(e.items()) for e in doc.getiterator() 
-    if e.items() != []]
+posts = [dict(e.items()) for e in doc.getiterator() if e.items() != []]
 df = DataFrame(posts)
 
 def decode_body(elem):
     try:
-        elem = elem.encode('utf-8')
-        return ''.join(BeautifulSoup(elem).findAll(text=True)
+        e = elem.encode('utf-8')
+        elem = "".join(BeautifulSoup(e).findAll(text=True))
     except AttributeError:
         # This means an empty value got through.
-        return None
+        elem = ""
+    return elem
+
+def utcify(date):
+    # Oddly enough, the timestamps are already in UTC format,
+    # except for a missing 'Z' at the end.
+    if type(date) == "str":
+        return "%sZ" % date
+    else:
+        return ""
+
+def parse_tags(tag):
+    # Tags are strings "<tag1><tag2-long><tag3-even-longer>".
+    try:
+        tag = tag.replace('<', ' ')
+        tag = tag.replace('>', ' ')
+        tags = [t.replace('-', ' ') for t in tag.split()]
+    except AttributeError:
+        # This means there are no tags provided.
+        tags = [""]
+
+    return tags
 
 df['Body'] = df['Body'].map(decode_body)
+df['CreationDate'] = df['CreationDate'].map(utcify)
+df['ClosedDate'] = df['ClosedDate'].map(utcify)
+df['Tags'] = df['Tags'].map(parse_tags)
+
+cols = ['Id', 'Body', 'ClosedDate', 'CreationDate', 'Title', 'Tags']
+
+df.to_csv('all.csv',
+    cols=cols,
+    index=False,
+    index_label=False,
+    encoding='utf-8')
